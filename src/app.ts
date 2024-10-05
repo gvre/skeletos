5967278935d6a18231b11316481f9eb7d8a47d37
Giannis Vrentzos <gvre@gvre.gr>
2024-10-05 19:28:49 +0300

    Add default error handler

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.disable('x-powered-by');
 // Get the user IP, instead of the proxy one.
 app.enable('trust proxy');
 
-// Logger middleware
+// Middlewares
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
   const clientIp = req.ip;
   const userAgent = req.get('User-Agent');
@@ -28,10 +28,26 @@ const corsOptions: CorsOptions = {
   exposedHeaders: ['X-Request-Id'],
 };
 app.use(cors(corsOptions));
-
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Routes
 app.use('/', router);
+
+// Default error handler
+router.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  // If you call next() with an error after you have started writing the response
+  // (for example, if you encounter an error while streaming the response to the client)
+  // the Express default error handler closes the connection and fails the request.
+  // So when you add a custom error handler, you must delegate to the default Express error handler,
+  // when the headers have already been sent to the client.
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  req.logger.error(err);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
 
 export { app };
